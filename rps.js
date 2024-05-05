3530e2462be24d5b02413d1a0ad1147edffc3f6a
Johnson274 <johnson.chris44@yahoo.com>
2024-05-04 20:42:25 -0500

    Add else/if statments to return rock, paper, or scissors based on value from getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
-function getComputerChoice(){
-    return Math.random();
+function getComputerChoice(X){
+    return Math.floor(Math.random() * X);
+    if (X===0) return "rock";
+    else if (X===1) return "paper";
+    else if (X===2) return "scissors"
 }
