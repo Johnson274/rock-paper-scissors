5c832078ed6b64739b30d80d35cfe820f2086252
Johnson274 <johnson.chris44@yahoo.com>
2024-05-05 21:38:16 -0500

    Edit getComupterChoice function so it works to return string answers properly

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
-function getComputerChoice(X){
-    return Math.floor(Math.random() * X);
-    if (X===0) return "rock";
-    else if (X===1) return "paper";
-    else if (X===2) return "scissors"
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber===0) return "rock";
+    else if (randomNumber===1) return "paper";
+    else if (randomNumber===2) return "scissors"
 }
