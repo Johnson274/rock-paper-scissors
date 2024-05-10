8d6f4fa40cf7752bd7ec9e8fd01817150335b747
Johnson274 <johnson.chris44@yahoo.com>
2024-05-09 20:36:51 -0500

    Capitalize first letter of all getcomputerChoice return values

--- rps.js
+++ rps.js
@@ -1,8 +1,8 @@
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber===0) return "rock";
-    else if (randomNumber===1) return "paper";
-    else if (randomNumber===2) return "scissors"
+    if (randomNumber===0) return "Rock";
+    else if (randomNumber===1) return "Paper";
+    else if (randomNumber===2) return "Scissors"
 }
 
 function getHumanChoice(){
