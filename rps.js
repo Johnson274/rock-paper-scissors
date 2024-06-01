c0850d16f6841e5227b75270e60bd6efc02e0e4c
Johnson274 <johnson.chris44@yahoo.com>
2024-06-01 15:45:14 -0500

    Remove extra n from checkWinner function spelling

--- rps.js
+++ rps.js
@@ -19,7 +19,7 @@ function getHumanChoice(){
     else if (finalUserInput === "scissors") return "Scissors";
 }
 
-function checkWinnner(humanChoice,computerChoice){
+function checkWinner(humanChoice,computerChoice){
 
     if (humanChoice === "rock" && computerChoice === "rock") return "It's a tie";
     else if (humanChoice === "rock" && computerChoice === "paper") return "You lose! Paper beats Rock";
